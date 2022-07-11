6f2544fd96d9a65c8b1d50c880872e647fd66464
Syed Naqvi <snaqvi21@hotmail.com>
2022-07-11 20:50:20 +0100

    creating_methods

--- directory.rb
+++ directory.rb
@@ -12,10 +12,23 @@ students.push("Dr. Hannibal Lecter",
   "Joffrey Baratheon",
   "Norman Bates")
 # print the array using .each method to iterate over each element of the array
-puts "The students of Villains Academy"
-puts "--------------------------------"
-students.each do |student|
-  puts student
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------------------------"
 end
-#finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+puts "Overall, we have #{names.count} great students"
+end
+
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
