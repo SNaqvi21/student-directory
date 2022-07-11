0e5e6696f474b69171191341a57c3dce63cd81c4
Syed Naqvi <snaqvi21@hotmail.com>
2022-07-11 20:35:33 +0100

    students_array&_do_block

--- directory.rb
+++ directory.rb
@@ -1,16 +1,21 @@
-student_count = 11
+students = Array.new # create an array object from the Array class
+# let's put all students into an array
+students.push("Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicker Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates")
+# print the array using .each method to iterate over each element of the array
 puts "The students of Villains Academy"
 puts "--------------------------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicker Witch of the West"
-puts "Terminator"
-puts " Freddy Kruger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts student
+end
 #finally, we print the total number of students
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
