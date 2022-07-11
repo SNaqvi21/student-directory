36e7d778ca1e7b1cd6355d1e639a9882387b1cf4
Syed Naqvi <snaqvi21@hotmail.com>
2022-07-11 21:22:19 +0100

    name/cohort_hashes

--- directory.rb
+++ directory.rb
@@ -1,16 +1,18 @@
 students = Array.new # create an array object from the Array class
 # let's put all students into an array
-students.push("Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicker Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates")
+students = [
+{name: "Dr. Hannibal Lecter", cohort: :november},
+{name: "Darth Vader", cohort: :november},
+{name: "Nurse Ratched", cohort: :november},
+{name: "Michael Corleone", cohort: :november},
+{name: "Alex DeLarge", cohort: :november},
+{name: "The Wicker Witch of the West", cohort: :november},
+{name: "Terminator", cohort: :november},
+{name: "Freddy Krueger", cohort: :november},
+{name: "The Joker", cohort: :november},
+{name: "Joffrey Baratheon", cohort: :november},
+{name: "Norman Bates", cohort: :november}
+]
 # print the array using .each method to iterate over each element of the array
 
 def print_header
@@ -18,9 +20,9 @@ def print_header
   puts "--------------------------------"
 end
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
