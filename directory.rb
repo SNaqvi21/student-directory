3183fcbf2488f33b468bfbb30f3b85fbdd17381d
Syed Naqvi <snaqvi21@hotmail.com>
2022-07-11 20:04:34 +0100

    comment_total_students

--- directory.rb
+++ directory.rb
@@ -11,6 +11,8 @@ puts " Freddy Kruger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#finally, we print the total number of students
 print "Overall, we have "
+#it's important that print() doesn't add a new line character
 print 11
 puts " great students"
