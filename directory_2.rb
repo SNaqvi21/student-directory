cb58c294472858e4d20e3e6498f23cab99ac3478
Syed Naqvi <snaqvi21@hotmail.com>
2022-07-13 12:58:33 +0100

    directory_2.rb

--- directory_2.rb
+++ directory_2.rb
@@ -1,22 +1,23 @@
-students = Array.new # create an array object from the Array class
+#students = Array.new # create an array object from the Array class
 # let's put all students into an array
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicker Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
+#students = [
+#{name: "Dr. Hannibal Lecter", cohort: :november},
+#{name: "Darth Vader", cohort: :november},
+#{name: "Nurse Ratched", cohort: :november},
+#{name: "Michael Corleone", cohort: :november},
+#{name: "Alex DeLarge", cohort: :november},
+#{name: "The Wicker Witch of the West", cohort: :november},
+#{name: "Terminator", cohort: :november},
+#{name: "Freddy Krueger", cohort: :november},
+#{name: "The Joker", cohort: :november},
+#{name: "Joffrey Baratheon", cohort: :november},
+#{name: "Norman Bates", cohort: :november}
+#]
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
+
   # create an empty array
   students = []
   # get the first name
@@ -41,11 +42,16 @@ end
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
 end
 
 def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+  if names.empty? == true
+    puts "No students entered"
+  elsif names.count == 1
+    puts "Overall, we have #{names.count} great student"
+  else
+    puts "Overall, we have #{names.count} great students"
+  end
 end
 
 #nothing happens until we call the methods
@@ -53,3 +59,4 @@ students = input_students
 print_header
 print(students)
 print_footer(students)
+end
